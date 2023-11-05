8d566bb657e0dbdc31a3b1e7f827223201f2ca31
Kim Min Jeong <dotoriido@gmail.com>
2023-11-06 01:21:58 +0900

    #4 - background image 흐리게 만들기

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import styles from '../styles/Detail.module.css';
+import {IoIosArrowBack} from 'react-icons/io';
+
 export default function Detail() {
+    const handleHeart=()=>{
+
+    }
     return (
-        <div>
+        <div className={styles.body}>
+            <div className={styles.arrow}><IoIosArrowBack /></div>
             <div className={styles.top}>
                 <img src={`${process.env.PUBLIC_URL}/assets/clothes.svg`} alt="" />
-                <img src={`${process.env.PUBLIC_URL}/assets/heart.svg`} alt="" />
+                <img onClick={handleHeart} src={`${process.env.PUBLIC_URL}/assets/heart.svg`} alt="" />
             </div>
             <div>
                 <div className={styles.bottom}>
-                    <img src={`${process.env.PUBLIC_URL}/assets/clothesDetail.svg`} alt="" />
-                    <img src={`${process.env.PUBLIC_URL}/assets/heart.svg`} alt="" />
+                    <div>
+                        <img src={`${process.env.PUBLIC_URL}/assets/clothesDetail.svg`} alt="" />
+                        <img onClick={handleHeart} src={`${process.env.PUBLIC_URL}/assets/heart.svg`} alt="" />
+                    </div>
                 </div>
             </div>
         </div>
