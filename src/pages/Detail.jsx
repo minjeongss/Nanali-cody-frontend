ac23f85fdbd83ad34d971839c261f50f97f9b6a6
Kim Min Jeong <dotoriido@gmail.com>
2023-11-08 02:55:16 +0900

    #4 - 전체 정렬 구현(blur 효과 제외)

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from '../styles/Detail.module.css';
 import {IoIosArrowBack} from 'react-icons/io';
+import { Link } from 'react-router-dom';
 
 export default function Detail() {
     const handleHeart=()=>{
@@ -8,17 +9,18 @@ export default function Detail() {
     }
     return (
         <div className={styles.body}>
-            <div className={styles.arrow}><IoIosArrowBack /></div>
+            <Link to="/home">
+                <div className={styles.arrow}><IoIosArrowBack /></div>
+            </Link>
             <div className={styles.top}>
-                <img src={`${process.env.PUBLIC_URL}/assets/clothes.svg`} alt="" />
-                <img onClick={handleHeart} src={`${process.env.PUBLIC_URL}/assets/heart.svg`} alt="" />
+                <img className={styles.clothes} src={`${process.env.PUBLIC_URL}/assets/clothes2.svg`} alt="" />
+                <img onClick={handleHeart} className={styles.heart} src={`${process.env.PUBLIC_URL}/assets/heart.svg`} alt="" />
             </div>
             <div>
+                {/*map 사용*/}
                 <div className={styles.bottom}>
-                    <div>
-                        <img src={`${process.env.PUBLIC_URL}/assets/clothesDetail.svg`} alt="" />
-                        <img onClick={handleHeart} src={`${process.env.PUBLIC_URL}/assets/heart.svg`} alt="" />
-                    </div>
+                    <img className={styles.clothesDetail}src={`${process.env.PUBLIC_URL}/assets/clothesDetail.svg`} alt="" />
+                    <img onClick={handleHeart} className={styles.heart} src={`${process.env.PUBLIC_URL}/assets/heart.svg`} alt="" />
                 </div>
             </div>
         </div>
