cc9c74e79a816da4bf1cf5032aad8c9851f32bdd
os0906 <rpdlaehfdl21@gmail.com>
2023-11-18 03:54:54 +0900

    FEAT LikeButton(500 errorㅠㅠ)Final Commit

--- src/components/ClothingList.jsx
+++ src/components/ClothingList.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const ImageList = ({ imageUrls }) => {
   const [selectedImages, setSelectedImages] = useState([]);
@@ -24,20 +25,58 @@ const ImageList = ({ imageUrls }) => {
       array[currentIndex] = array[randomIndex];
       array[randomIndex] = temporaryValue;
     }
-
     return array;
   };
 
+  const handleLikeClick = async (garmentId) => {
+    try {
+      // Check if the garment is already liked
+      const isLiked = selectedImages.some((image) => image.garmentId === garmentId);
+
+      if (isLiked) {
+        // Unlike the garment
+        await axios.delete(`http://3.37.139.209:8080/api/like/garment/${garmentId}`);
+        setSelectedImages((prevImages) => prevImages.filter((image) => image.garmentId !== garmentId));
+      } else {
+        // Like the garment
+        await axios.post(`http://3.37.139.209:8080/api/like/garment/${garmentId}`);
+        // Assuming garmentId is a unique identifier for each image
+        // If not, you may need to adjust this logic based on your data structure
+        setSelectedImages((prevImages) => [...prevImages, { garmentId }]);
+      }
+    } catch (error) {
+      console.error('Error toggling like:', error);
+      // Handle error, e.g., show an error message to the user
+    }
+  };
   return (
     <div>
       <div className = "imglist" style={{ display: 'flex' ,alignItems:'center'}}>
         {selectedImages.map((imageUrl, index) => (
-          <img
-            key={index}
-            src={imageUrl.imgUrl}
-            alt={`Image ${index + 1}`}
-            style={{ width: '120px', height: '120px', marginRight: '11px', borderRadius:'25px' }}
-          />
+         <div key={imageUrl.id} style={{ position: 'relative', marginRight: '11px' }}>
+            <img
+              src={imageUrl.imgUrl}
+              alt={`Image ${index + 1}`}
+              style={{ width: '120px', height: '120px', borderRadius: '25px' }}
+            />
+            <button
+              onClick={() => handleLikeClick(imageUrl.id)}
+              style={{
+                position: 'absolute',
+                top: '0',
+                right: '0',
+                height:'15px',
+                width:'15px',
+                backgroundImage:`url(${process.env.PUBLIC_URL}/assets/unLike.svg)` ,
+                border: 'none',
+                cursor: 'pointer',
+              }}
+            >
+              {selectedImages.some((likedImage) => likedImage.garmentId === imageUrl.id)
+                ? 'Unlike'
+                : 'Like'}
+            </button>
+          </div>
         ))}
       </div>
     </div>
