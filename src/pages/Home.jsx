c03b0ae16dd40c553a9f8e329039138166ad50c6
Kim Min Jeong <dotoriido@gmail.com>
2023-11-18 07:21:57 +0900

    #3 - axios로 정보 가져오기 | 새로고침 누르면 새로운 옷 가져오기

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,22 +12,31 @@ export default function Home() {
     //     {}
     // ]);
     // const dateList=calendar.map((list)=><div></div>);
-    const [isDay,setIsDay]=useState(false);
+    const [isDay,setIsDay]=useState(true);
     const [isRainy,setIsRainy]=useState(false);
-    const [rainPercent,setRainPercent]=useState(100);
+    const [rainPercent,setRainPercent]=useState(10);
     const [weather,setWeather]=useState("cloud");
     const [uv,setUv]=useState(2);
+    const [clothUrl,setClothUrl]=useState();
+    const [body,setBody]=useState();
+    const [time,setTime]=useState([]);
+    const [tem,setTem]=useState();
 
-    useEffect(()=>{
-        axios.get(`/api/outfit`)
+    const fetchData=()=>{
+        axios.get(`http://3.37.139.209:8080/api/outfit?style=DANDY&sex=MAN&time=2023-11-18T10:07:00`)
         .then((response)=>{ //실제 구동
-            //setData(response.data)
-            //console.log(response)
-            alert(response);
+            setData(response.data);
+            setClothUrl(response.data.outfitUrl);
+            setBody(response.data.weather.body);
+            setTime(Object.keys(body));
+            setTem(response.data.weather.body.get([Object.keys(body)[0]]));
         })
         .catch((error)=>{ //error처리
             console.log(error)
         })
+    }
+    useEffect(()=>{
+        fetchData();
     },[])
     
     const PrintUmbrella=()=>{
@@ -49,7 +58,7 @@ export default function Home() {
         return null;
     }
     const handleRandom=()=>{
-
+        fetchData();
     }
     const handleButtonSex=()=>{
         
@@ -58,8 +67,8 @@ export default function Home() {
         <div className={isDay?`${styles.bodyDay}`:`${styles.bodyNight}`}>
             <div className={styles.top}>
                 <div className={styles.topLeft}>
-                    <div className={styles.date}>10월 18일 수요일</div>
-                    <div className={isDay?`${styles.temperatureDay}`:`${styles.temperatureNight}`}>11°</div>
+                    <div className={styles.date}>11월 18일 토요일</div>
+                    <div className={isDay?`${styles.temperatureDay}`:`${styles.temperatureNight}`}>2°</div>
                     <div className={isDay?`${styles.locationDay}`:`${styles.locationNight}`}><button className={styles.buttonImgSmall}><img src={`${process.env.PUBLIC_URL}/assets/location.svg`} alt="" /></button>인천, 대한민국</div>
                     <div className={styles.rainyPercent}>강수확률2% {PrintUmbrella()}</div>
                 </div>
@@ -75,9 +84,8 @@ export default function Home() {
                     <div>오늘은 이 옷 어때요?</div>
                         <img onClick={handleRandom} className={styles.imgCenterTitle} src={isDay?`${process.env.PUBLIC_URL}/assets/reset.svg`:`${process.env.PUBLIC_URL}/assets/resetDark.svg`} alt="" />
                     <Link to="/detail">
-                        <img src={`${process.env.PUBLIC_URL}/assets/clothes.svg`} alt="" />
+                        <img className={styles.clothes} src={clothUrl} alt="" />
                     </Link>
-                    <div>옷 정보</div>
                 </div>
             </div>
             <div className={styles.bottom}>
@@ -87,13 +95,12 @@ export default function Home() {
                 </div>
             </div>
             <div className={styles.swipe}>
-                    <SwiperWeather />
-                    <SwiperWeather />
-                    <SwiperWeather />
-                    <SwiperWeather />
-                    <SwiperWeather />
-                    <SwiperWeather />
-                    <SwiperWeather />
+                    {/* <SwiperWeather time={data.weather}/> */}
+                    <div className={styles.box}>
+                        <div>{time[0]}</div> {/*11부터 시간 시작*/}
+                        <img className={styles.weather} src={`${process.env.PUBLIC_URL}/assets/cloud.svg`} alt="" />
+                        <div>{tem}</div>
+                    </div>
                     <SwiperWeather />
                     <SwiperWeather />
                 </div>
